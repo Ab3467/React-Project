2112adb8e93a5a62ae38d1073a337cd38a5ba2f5
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-01 15:59:21 +0500

    changes

--- my-app/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js}"], // Include JS and JSX files in all subdirectories of src
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
