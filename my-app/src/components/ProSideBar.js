fb803c1ed447cab31c537d463a923fe6e102ad33
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-01 15:01:22 +0500

    Making components

---
+++ my-app/src/components/ProSideBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function ProSideBar() {
+  return (
+    <div>
+      <aside>
+        <h2>Your Projects</h2>
+        <div>
+            <button>
+            + Add Project
+            </button>
+        </div>
+            <ul>
+
+            </ul>
+        
+      </aside>
+    </div>
+  )
+}
