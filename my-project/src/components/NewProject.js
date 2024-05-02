8d6e294a09ca2afdc36eb5fe1d06ba2ac7439a4b
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-02 17:48:49 +0500

    Changes

--- my-project/src/components/NewProject.js
+++ my-project/src/components/NewProject.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React,{useState,useRef} from 'react'
 import Input from '../Input'
 
 export default function NewProject() {
+
+
   return (
     <>
     <div className='w-[35rem] mt-16'>
         <menu className='flex items-center justify-end gap-4 my-4'>
             <li><button className='text-stone-800 hover:text-stone-950'>Cancel</button></li>
-            <li><button className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'>Save</button> </li>
+            <li><button onClick={HandleSaveButton} value={saveBtn} className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'>Save</button> </li>
         </menu> 
         <div>
             <Input label={"Title"} />
