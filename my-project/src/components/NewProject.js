08afc150b63b9dcfc00c869d4bb060733a8c72c1
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-02 18:06:36 +0500

    Changes

--- my-project/src/components/NewProject.js
+++ my-project/src/components/NewProject.js
@@ -2,7 +2,17 @@ import React,{useState,useRef} from 'react'
 import Input from '../Input'
 
 export default function NewProject() {
+const Title = useRef();
+const Description = useRef();
+const Duedate = useRef();
 
+
+
+function HandleSaveButton(){
+ const EnteredTitle = Title.current.value;
+ const EnteredDes= Description.current.value;
+ const EnteredDueD = Duedate.current.value;
+}
 
   return (
     <>
@@ -12,9 +22,9 @@ export default function NewProject() {
             <li><button onClick={HandleSaveButton} value={saveBtn} className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'>Save</button> </li>
         </menu> 
         <div>
-            <Input label={"Title"} />
-            <Input label={"Description"} textarea/>
-            <Input label={"Due Date"}/>
+            <Input ref={Title} label={"Title"}/>
+            <Input ref={Description} label={"Description"} textarea/>
+            <Input ref={Duedate} label={"Due Date"}/>
         </div>
     </div>
     </>
