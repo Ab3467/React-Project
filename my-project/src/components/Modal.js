1320951debc8493eb7009cceb34ac4812cbbd884
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-02 19:14:11 +0500

    Changes

--- my-project/src/components/Modal.js
+++ my-project/src/components/Modal.js
@@ -1,8 +1,9 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom"
+import Button from "./Button";
 
 
-const Modal = forwardRef(function Modal({children},ref) {
+const Modal = forwardRef(function Modal({children,btnCaption},ref) {
     const dialog = useRef();
 
     useImperativeHandle(ref,()=>{
@@ -14,8 +15,13 @@ const Modal = forwardRef(function Modal({children},ref) {
     });
 
   return createPortal(
-  <dialog ref={dialog}>
+  <dialog ref={dialog} className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md">
     {children}
+    <form method="dialog" className="mt-4 text-right">
+        <Button>
+            {btnCaption}
+        </Button>
+    </form>
   </dialog>,document.getElementById('modal-root')
   );
 })
--- my-project/src/components/NewProject.js
+++ my-project/src/components/NewProject.js
@@ -22,6 +22,7 @@ function HandleSaveButton(){
     EnteredDueD.trim() ===''
     ){
       modal.current.open();
+      return;
     }
 
  onAdd({
@@ -33,7 +34,12 @@ function HandleSaveButton(){
 
   return (
     <>
-    <Modal ref={modal}/>
+    <Modal ref={modal} btnCaption="Ok">
+      <h2 className='text-xl font-bold text-stone-700 my-4'>Invalid Input</h2>
+      <p className='text-stone-600 mb-4'>OOPs... Looks like you forget to enter a value</p>
+      <p className='text-stone-600 mb-4'>Please Make sure you provided a valid value for every input field</p>
+    </Modal>
+
     <div className='w-[35rem] mt-16'>
         <menu className='flex items-center justify-end gap-4 my-4'>
             <li><button className='text-stone-800 hover:text-stone-950'>Cancel</button></li>
