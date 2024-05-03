24e6be8a01faa1dca48c38e4d47c0f1da362e283
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-03 17:43:15 +0500

    Changes

--- my-project/src/components/Tasks.js
+++ my-project/src/components/Tasks.js
@@ -6,7 +6,7 @@ export default function Tasks() {
     <section>
       <h2 className='text-2xl font-bold text-stone-700 mb-4'>Tasks</h2>
        <NewTask/>
-      <p className='text-stone-800 mb-4'>This project does not have any task yes!</p>
+      <p className='text-stone-800 my-4'>This project does not have any task yes!</p>
       <ul></ul>
     </section>
   );
